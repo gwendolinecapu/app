158d9e8bce8e0479d4898a5e21dbbf6aebbfe673
gwendolinecapu <gwendolinecapu@gmail.com>
2025-12-30 23:31:35 +0100

    updates

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -9,6 +9,7 @@ import {
     Modal,
     FlatList,
     RefreshControl,
+    Image,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { router } from 'expo-router';

4701ab99e1336bcb798f7474809cb2d22c4fa549
gwendolinecapu <gwendolinecapu@gmail.com>
2026-01-11 19:40:03 +0100

    updates

--- plugins/withModularHeaders.js
+++ plugins/withModularHeaders.js
@@ -10,48 +10,44 @@ const withModularHeaders = (config) => {
       if (fs.existsSync(podfile)) {
         let contents = fs.readFileSync(podfile, 'utf-8');
 
-        // 1. Ensure use_modular_headers! isn't prepended (we'll use selective pre_install)
+        // 1. CLEANUP: Remove any previous failed attempts
+        // Remove the global prepend if it's there
         contents = contents.replace(/^use_modular_headers!\n/, "");
+        // Remove any pre_install block entirely (even with nested ends)
+        // We use a more aggressive but limited search
+        contents = contents.replace(/\n\s*pre_install do \|installer\|[\s\S]*?FirebaseCoreInternal[\s\S]*?end\s*end\s*end/, "");
+        contents = contents.replace(/\n\s*pre_install do \|installer\|[\s\S]*?FirebaseCoreInternal[\s\S]*?end\s*end/, "");
+        // Clean up the dangling ends specifically
+        contents = contents.replace(/prepare_react_native_project!\n\s*end\s*\n\s*end/, "prepare_react_native_project!");
 
-        // 2. Clear old custom pre_install blocks to avoid duplication
-        contents = contents.replace(/\n\s*pre_install do \|installer\|[\s\S]*?end\n/g, "\n");
+        // 2. APPLY CORRECT GLOBAL SETTING
+        if (!contents.includes('use_modular_headers!')) {
+          contents = "use_modular_headers!\n" + contents;
+        }
 
-        // 3. Selective pre_install
-        const preInstallCode = `
-  pre_install do |installer|
-    installer.pod_targets.each do |pod|
-      if ['FirebaseCoreInternal', 'GoogleUtilities', 'MediaPipeTasksGenAIC', 'expo-llm-mediapipe'].include?(pod.name)
-        pod.use_modular_headers = true
-      end
-    end
-  end
-`;
-        contents = contents.replace(/target 'PluralConnect' do/, preInstallCode + "\ntarget 'PluralConnect' do");
-
-        // 4. Robust post_install injection
-        // We'll search for the existing post_install and inject our settings at the very beginning of it
+        // 3. APPLY AGGRESSIVE POST_INSTALL
         const buildSettingsCode = `
     installer.pods_project.targets.each do |target|
       target.build_configurations.each do |config|
         config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+        config.build_settings['OTHER_CFLAGS'] ||= ['$(inherited)']
+        config.build_settings['OTHER_CFLAGS'] << '-Wno-non-modular-include-in-framework-module'
+        config.build_settings['OTHER_CPLUSPLUSFLAGS'] ||= ['$(inherited)']
+        config.build_settings['OTHER_CPLUSPLUSFLAGS'] << '-Wno-non-modular-include-in-framework-module'
         if target.name.start_with?('RNFBApp') || target.name.start_with?('RNFBAnalytics')
            config.build_settings['DEFINES_MODULE'] = 'NO'
         end
       end
     end`;
 
-        // If our block isn't there, add it
-        if (!contents.includes('CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES')) {
-          contents = contents.replace(
-            /post_install do \|installer\|/,
-            `post_install do |installer|${buildSettingsCode}`
-          );
-        } else {
-          // If it is there, replace the old block with the new one (with DEFINES_MODULE)
-          contents = contents.replace(
-            /installer\.pods_project\.targets\.each do \|target\|[\s\S]*?end\s*end/,
-            buildSettingsCode
-          );
+        if (contents.includes('post_install do |installer|')) {
+          // Check if we already injected our block
+          if (!contents.includes('CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES')) {
+            contents = contents.replace(
+              /post_install do \|installer\|/,
+              `post_install do |installer|${buildSettingsCode}`
+            );
+          }
         }
 
         fs.writeFileSync(podfile, contents);

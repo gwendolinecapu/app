2b81a91b63eaeeb0499349529e975f2b6fcd9732
Wartail <leo.usquelis@gmail.com>
2026-01-26 19:47:39 +0100

    fix: Rename `systemId` to `system_id` in Firestore rules and disable `DismissKeyboard` functionality on web platforms.

--- src/components/ui/DismissKeyboard.tsx
+++ src/components/ui/DismissKeyboard.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Keyboard, TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
+import { Keyboard, TouchableWithoutFeedback, View, StyleSheet, Platform } from 'react-native';
 
 interface DismissKeyboardProps {
     children: React.ReactNode;
 }
 
-export const DismissKeyboard = ({ children }: DismissKeyboardProps) => (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-        <View style={styles.container}>
-            {children}
-        </View>
-    </TouchableWithoutFeedback>
-);
+export const DismissKeyboard = ({ children }: DismissKeyboardProps) => {
+    if (Platform.OS === 'web') {
+        return <View style={styles.container}>{children}</View>;
+    }
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <View style={styles.container}>
+                {children}
+            </View>
+        </TouchableWithoutFeedback>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
